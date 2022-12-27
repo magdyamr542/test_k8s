69ad6e87fa0550523997fd91b51820e3e8766c60
Amr Metwally <amr.metwally@green-connector.de>
2022-12-27 20:18:00 +0100

    Testing the ingress

---
+++ ingress/service1/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+	seed := rand.Intn(1000)
+
+	http.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("[Service1] Request on node %d\n", seed)
+		w.Write([]byte(fmt.Sprintf("Data from service1 node %d\n", seed)))
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("[Service1] Request on node %d path '%s'\n", seed, r.URL.Path)
+		w.Write([]byte(fmt.Sprintf("[Service1] Ok on node %d\n", seed)))
+	})
+
+	fmt.Printf("Current node %v\n", seed)
+	fmt.Printf("Server is up on port 1234\n")
+	http.ListenAndServe(":1234", nil)
+}
